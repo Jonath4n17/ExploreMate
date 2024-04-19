d8bde6fb4db68bddfbd5bc79e99d1130919b38c2
jonathanLin <jonathan.lin1104@gmail.com>
2024-04-19 16:12:05 -0400

    Initial Commit

---
+++ ExploreMate/ExploreMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreMateApp.swift
+//  ExploreMate
+//
+//  Created by Jodie Zhu on 2024-04-19.
+//
+
+import SwiftUI
+
+@main
+struct ExploreMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
